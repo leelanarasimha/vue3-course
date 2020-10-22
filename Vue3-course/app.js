f8f99281f80091043b15dd6d8d5b045d0a2762cc
Leela Narasimha <leelanrp@qualminds.com>
2020-10-22 18:23:14 +0530

    Video-5 Reference Code

--- Vue3-course/app.js
+++ Vue3-course/app.js
@@ -2,8 +2,18 @@ const app = Vue.createApp({
     data() {
         return {
             title: 'Create a Post Data',
+            text1: '<h1>Learn Vue</h1>',
+            text2: 'Master Vue',
             link: 'https://vuejs.org'
         }
+    },
+    methods: {
+        getText() {
+            return this.text1;
+        },
+        getAnotherText() {
+            return this.text2;
+        }  
     }
 });
 
