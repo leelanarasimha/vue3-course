569477b292d683975e287aef54d5e6c389807750
Leela Narasimha <leelanrp@qualminds.com>
2020-10-24 22:39:14 +0530

    Added Event Modifiers

--- Vue3-eventBinding/app.js
+++ Vue3-eventBinding/app.js
@@ -6,6 +6,9 @@ const app = Vue.createApp({
         };
     },
     methods: {
+        submitForm() {
+            alert('submitted');
+        },
         onIncrement() {
             this.count++;
         },
