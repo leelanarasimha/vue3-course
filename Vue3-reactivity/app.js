4ec3081a3d65d834fd203b391a108fcbff05354b
Leela Narasimha <leela.narsimha@gmail.com>
2020-10-27 23:08:18 +0530

    On app vs multiple apps

---
+++ Vue3-reactivity/app.js
@@ -0,0 +1,30 @@
+const commonData = {
+    commonMessage: 'This is the common message',
+};
+
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'Hello world',
+            data: commonData,
+        };
+    },
+});
+
+app.mount('#app');
+
+const app2 = Vue.createApp({
+    data() {
+        return {
+            anotherMessage: 'Hello another world',
+            data: commonData,
+        };
+    },
+    methods: {
+        changeMessage() {
+            this.data.commonMessage = 'Changed the message';
+        },
+    },
+});
+
+app2.mount('#app2');
