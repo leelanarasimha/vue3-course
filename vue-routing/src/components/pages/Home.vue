a0e2a2047e756b24664798fd0f2e502646240988
Leela Narasimha <leela.narsimha@gmail.com>
2020-11-08 08:38:16 +0530

    Added $router push

--- vue-routing/src/components/pages/Home.vue
+++ vue-routing/src/components/pages/Home.vue
@@ -1,9 +1,21 @@
 <template>
     <div>
-        Home Page
+        <div>
+            <button @click.prevent="onPostsClick()">
+                CLick to Posts Page
+            </button>
+        </div>
+        <div>Home Page</div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    methods: {
+        onPostsClick() {
+            this.$router.push('/posts');
+            //this.$router.go(1);
+        },
+    },
+};
 </script>
