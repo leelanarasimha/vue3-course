4c266609425e29b900b1033ae5c57d5fcf7695d0
Leela Narasimha <leela.narsimha@gmail.com>
2020-11-11 09:07:25 +0530

    Added clased based animations

--- vue-animations/src/App.vue
+++ vue-animations/src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box" :class="{ animate: animate }"></div>
+                <div class="my-3">
+                    <button
+                        class="btn btn-primary"
+                        @click.prevent="onAnimateClick()"
+                    >
+                        Click Me
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            animate: false,
+        };
+    },
+    methods: {
+        onAnimateClick() {
+            this.animate = true;
+        },
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.box {
+    width: 100px;
+    height: 100px;
+    border: 1px solid red;
+    background: red;
+}
+
+.animate {
+    animation: animate-box 1s forwards;
+}
+
+@keyframes animate-box {
+    0% {
+        transform: translateX(0px) scaleX(1);
+    }
+    50% {
+        transform: translateX(100px) scaleX(1.5);
+    }
+    100% {
+        transform: translateX(200px) scaleX(1);
+    }
 }
 </style>
