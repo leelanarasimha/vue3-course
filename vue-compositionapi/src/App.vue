2391f62176427562268122d4263a8966c2191bed
Leela Narasimha <leela.narsimha@gmail.com>
2020-11-30 07:54:29 +0530

    created the composition api

---
+++ vue-compositionapi/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div>The name of the youtube channel is {{ userName }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userName: 'Leela Web Dev',
+        };
+    },
+
+    mounted() {
+        setTimeout(() => {
+            this.userName = 'Leela';
+        }, 3000);
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
